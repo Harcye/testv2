8a16d46d1775fc52c9109c094ca8d1bf44e0a448
Yuericy <harveycompanystart@gmail.com>
2025-03-03 17:07:53 +0200

    Update Auptabes.lua

--- Auptabes.lua
+++ Auptabes.lua
@@ -12,12 +12,15 @@ local distatus = require "moonloader".download_status
 encoding.default = "CP1251"
 u8 = encoding.UTF8
 
-local update_state = false
+-- Настройки скрипта
 local script_vers = 2
-local script_vers_text = "1.05"
-local update_url = "https://raw.githubusercontent.com/Harcye/testv2/refs/heads/main/uptabe.ini"
+local script_vers_text = "1.00"
+local update_url = "https://raw.githubusercontent.com/Harcye/testv2/main/uptabe.ini"
+local script_url = "https://raw.githubusercontent.com/Harcye/testv2/main/Auptabes.lua"
+
+-- Пути к файлам
 local update_path = getWorkingDirectory() .. "/update.ini"
-local script_url = "https://raw.githubusercontent.com/Harcye/testv2/refs/heads/main/Auptabes.lua"
+local temp_path = getWorkingDirectory() .. "/Auptabes_tmp.lua"
 local script_path = thisScript().path
 
 function main()
@@ -28,32 +31,46 @@ function main()
     check_for_update()
 end
 
+-- Проверка наличия обновлений
 function check_for_update()
     downloadUrlToFile(update_url, update_path, function(id, status)
         if status == distatus.STATUS_ENDDOWNLOADDATA then
             local update_ini = inicfg.load(nil, update_path)
-            if tonumber(update_ini.info.vers) > script_vers then
-                sampAddChatMessage("Доступно обновление: " .. update_ini.info.vers, -1)
-                update_state = true
+            if update_ini and update_ini.info and tonumber(update_ini.info.vers) then
+                if tonumber(update_ini.info.vers) > script_vers then
+                    sampAddChatMessage("Доступно обновление: версия " .. update_ini.info.vers, 0x00FF00)
+                    download_script()
+                else
+                    sampAddChatMessage("У вас установлена последняя версия скрипта.", 0x00FF00)
+                end
+            else
+                sampAddChatMessage("Ошибка: некорректные данные в update.ini", 0xFF0000)
             end
             os.remove(update_path)
-            if update_state then download_script() end
         end
     end)
 end
 
+-- Загрузка и замена скрипта
 function download_script()
-    local temp_path = getWorkingDirectory() .. "/Auptabes_tmp.lua"
     downloadUrlToFile(script_url, temp_path, function(id, status)
         if status == distatus.STATUS_ENDDOWNLOADDATA then
-            os.remove(script_path) -- удаляем старый скрипт
-            os.rename(temp_path, script_path) -- заменяем на новый
-            sampAddChatMessage("Скрипт успешно обновлен! Перезагрузка...", -1)
-            thisScript():reload()
+            local file = io.open(temp_path, "r")
+            if file then
+                file:close()
+                os.remove(script_path) -- удаляем старый скрипт
+                os.rename(temp_path, script_path) -- заменяем на новый
+                sampAddChatMessage("Скрипт успешно обновлён! Перезагрузка...", 0x00FF00)
+                thisScript():reload()
+            else
+                sampAddChatMessage("Ошибка при загрузке скрипта.", 0xFF0000)
+            end
         end
     end)
 end
 
+-- Команда для ручного обновления
 function cmd_update()
-    sampShowDialog(1000, "Автообновление 2.0", "{FFFFFF} Это урок по обновлению\n{FFFF00} Новая версия Алекс Чай доступна к загрузке", "Закрыть", "", 0)
+    check_for_update()
+    sampAddChatMessage("Проверка обновлений алекс ...", 0xFFFF00)
 end
